ed7b738ccaff406c42e41f63764898be6ab34874
Amadea <53259552+amadea-system@users.noreply.github.com>
2025-03-17 13:25:54 -0400

    Formatting changes

--- core/config_tab_ui.lua
+++ core/config_tab_ui.lua
@@ -26,23 +26,23 @@ local mod = SMODS.current_mod
 
 
 -------------------- Helper Functions ---------------------
----
+
 ----- UI stuff
 
 -- from https://gist.github.com/GabrielBdeC/b055af60707115cbc954b0751d87ec23
 function string:split(delimiter)
-    local result = {}
-    local from = 1
-    local delim_from, delim_to = string.find(self, delimiter, from, true)
-    while delim_from do
-        if (delim_from ~= 1) then
-            table.insert(result, string.sub(self, from, delim_from-1))
-        end
-        from = delim_to + 1
-        delim_from, delim_to = string.find(self, delimiter, from, true)
-    end
-    if (from <= #self) then table.insert(result, string.sub(self, from)) end
-    return result
+	local result = {}
+	local from = 1
+	local delim_from, delim_to = string.find(self, delimiter, from, true)
+	while delim_from do
+		if (delim_from ~= 1) then
+			table.insert(result, string.sub(self, from, delim_from-1))
+		end
+		from = delim_to + 1
+		delim_from, delim_to = string.find(self, delimiter, from, true)
+	end
+	if (from <= #self) then table.insert(result, string.sub(self, from)) end
+	return result
 end
 
 local function create_keybind_button(args)
